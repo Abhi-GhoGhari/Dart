626279b6cef0d12c5deebfb75c9052563ef1860d
ABHI GHOGHARI <143987211+Abhi-GhoGhari@users.noreply.github.com>
2024-01-27 10:56:02 +0530

    Add files via upload

---
+++ core_flutter/PR-3/Q-1.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+int Sum({required List<int> list}){
+  int sum = 0;
+  list.forEach((element) {
+    sum += element;
+  });
+  return sum;
+}
+void main(){
+  List<int> myList = List.generate(5, (index) => index);
+
+  myList.forEach((element) {
+    stdout.write("ENTER NUMBER [${myList.indexOf(element)}] : ");
+    element = int.parse(stdin.readLineSync() ?? "0");
+  });
+  print("ENTERED NUMBER SUM =   ${Sum(list: myList)}\n");
+}
